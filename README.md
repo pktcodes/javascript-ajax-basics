0b4e23e8b6968f79940fe1c4d764c80632dc2b87
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-04-16 18:46:00 -0500

    Simple Text

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # AJAX
 
 Asynchronous JavaScript and XML
+
+HTTP Requests - Communication between Client and Server
+
+API - Application Programming Interface
+
+- Set of Instructions to work with Server i.e GET, POST, PUT, DELETE resource from the server.
--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("AJAX");
+const xhr = new XMLHttpRequest();
+
+xhr.open("GET", "./api/sample.txt");
+
+/* It gets fired when since ready state changes from 0 to 1 when used open() method */
+xhr.onreadystatechange = function () {
+  // console.log(xhr);
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    const paragraph = document.createElement("p");
+    paragraph.textContent = xhr.responseText;
+    document.body.appendChild(paragraph);
+  } else {
+    console.log({
+      status: xhr.status,
+      statusText: xhr.statusText,
+      state: xhr.readyState,
+    });
+  }
+};
+
+/* It sends the request to server, then it displays on the browser */
+xhr.send();
+
+// It executes first, since the above functionality works asynchronously by browser, only when JavaScript is free then it takes the data from browser
+console.log("hello world");
