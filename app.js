92e052736558c0a99e75b9c7fae1585b26d7f0e4
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-04-16 19:11:01 -0500

    Add Button

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
-const xhr = new XMLHttpRequest();
+/* To show that data fetching is happening asynchronously (in the background), but not on initial page refresh */
 
-xhr.open("GET", "./api/sample.txt");
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", () => {
+  getData();
+});
 
-/* It gets fired when since ready state changes from 0 to 1 when used open() method */
-xhr.onreadystatechange = function () {
-  // console.log(xhr);
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    const paragraph = document.createElement("p");
-    paragraph.textContent = xhr.responseText;
-    document.body.appendChild(paragraph);
-  } else {
-    console.log({
-      status: xhr.status,
-      statusText: xhr.statusText,
-      state: xhr.readyState,
-    });
-  }
-};
+function getData() {
+  const xhr = new XMLHttpRequest();
 
-/* It sends the request to server, then it displays on the browser */
-xhr.send();
+  xhr.open("GET", "./api/sample.txt");
+
+  /* It gets fired when since ready state changes from 0 to 1 when used open() method */
+  xhr.onreadystatechange = function () {
+    // console.log(xhr);
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const paragraph = document.createElement("p");
+      paragraph.textContent = xhr.responseText;
+      document.body.appendChild(paragraph);
+    } else {
+      console.log({
+        status: xhr.status,
+        statusText: xhr.statusText,
+        state: xhr.readyState,
+      });
+    }
+  };
+
+  /* It sends the request to server, then it displays on the browser */
+  xhr.send();
+}
 
 // It executes first, since the above functionality works asynchronously by browser, only when JavaScript is free then it takes the data from browser
 console.log("hello world");
