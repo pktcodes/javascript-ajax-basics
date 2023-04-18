99e18ba442cd949516220dfa78f00eaef8005576
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-04-17 23:11:53 -0500

    Fetch - Big Picture

--- app.js
+++ app.js
@@ -2,10 +2,22 @@
 // promised based
 // XHR is not wrong, you can complete all our upcoming examples and projects using XHR. Fetch is just alternative approach that has simpler and cleaner syntax. The end result is the same.Still get dynamically, behind the scenes.
 
-const url = "./api/people.jsons";
+const url = "./api/people.json";
 
-/* Using Implicit Return */
+/* Fetch is a builtin functionality that makes to do less work for setup so we can start implementating */
 fetch(url)
   .then((response) => console.log(response))
   .then((data) => console.log(data))
   .catch((error) => console.log(error));
+
+const promise = new Promise((resolve, reject) => {
+  let value = false;
+  if (value) {
+    resolve([1, 2, 5]);
+  } else {
+    reject(new Error(`The value is ${value}`));
+  }
+  return promise;
+});
+
+promise.then((data) => console.log(data)).catch((error) => console.log(error));
